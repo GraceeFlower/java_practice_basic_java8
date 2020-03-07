ff4c7a085d98cb913352c810b94e3d10ca166814
GraceeFlower <1491644103@qq.com>
2020-03-07 21:42:51 +0800

    [WM] feat: check whether the data exits by master number in service

--- JAVA8homework/src/main/java/PersonSet.java
+++ JAVA8homework/src/main/java/PersonSet.java
@@ -3,7 +3,10 @@ import entity.Email;
 import entity.MasterNumber;
 import entity.Person;
 import entity.Telephone;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -30,8 +33,18 @@ public class PersonSet {
     // TODO: group the data to Stream<Person>
     // Can use Collectors.groupingBy method
     // Can add helper method
+    Map<String, List<Address>> addrMap = this.addresses
+        .stream().collect(Collectors.groupingBy(Address::getMasterNumber));
+    Map<String, List<Telephone>> telephoneMap = this.telephones
+        .stream().collect(Collectors.groupingBy(Telephone::getMasterNumber));
+    Map<String, List<Email>> emailMap = this.emails
+        .stream().collect(Collectors.groupingBy(Email::getMasterNumber));
 
-   return null;
+    return this.masterNumbers.stream().map(MasterNumber::getNumber)
+        .map(num -> new Person(num,
+            telephoneMap.containsKey(num) ? telephoneMap.get(num) : new ArrayList<>(),
+            addrMap.containsKey(num) ? addrMap.get(num).get(0) : null,
+            emailMap.containsKey(num) ? emailMap.get(num) : new ArrayList<>()));
   }
 
   public List<Address> getAddresses() {
